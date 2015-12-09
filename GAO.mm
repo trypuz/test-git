550f9f307ec6b2b5b4d83ebf948fe672fe4d1d01
Robert Trypuz <robert@trypuz.pl>
2015-12-09 14:41:44 +0100

    plik GAO.mm

--- GAO.mm
+++ GAO.mm
@@ -1,3 +1,4 @@
+To jest moja nowa linia.
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1438082522395" ID="ID_1060502317" MODIFIED="1439198520244" TEXT="GAO">
